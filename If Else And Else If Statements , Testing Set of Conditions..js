aa4203957764d950a86f6735beca37b3a2d80dca
Iqra Mansha <115552409+IqraaMansha@users.noreply.github.com>
2023-07-01 21:49:16 +0500

    Update If Else And Else If Statements , Testing Set of Conditions..js

--- If Else And Else If Statements , Testing Set of Conditions..js
+++ If Else And Else If Statements , Testing Set of Conditions..js
@@ -2,6 +2,7 @@
 // If Else And Else If Statements,
 // Testing Set of Conditions.
 // Q.no.1
+
 var a = prompt("Enter a single character");
 if (a.charCodeAt() >= 65 && a.charCodeAt() <= 90) {
     alert("Capital Letter");
